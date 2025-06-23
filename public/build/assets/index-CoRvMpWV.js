5620478963d6b932e9e402ee096c36a7bc2733c6
sunilhavensoftwares <sunilhavensoftwares@gmail.com>
2025-06-23 17:24:08 +0530

    run build

---
+++ public/build/assets/index-CoRvMpWV.js
@@ -0,0 +1,1 @@
+import{a as r}from"./app-BsW0TKv2.js";var o=r();export{o as r};
--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -2,8 +2,24 @@ import { useEffect } from 'react';
 import  './css/guestLayout.css';
 import useScript from '@/Hooks/useScript';
 import '@assets/js/custom/authentication/sign-in/general.js';
+import guest_body_bg from '@assets/media/auth/bg4.jpg';
 export default function Guest({ children, bodyClass = 'app-blank bgi-size-cover bgi-attachment-fixed bgi-position-center bgi-no-repeat guestLayoutBody' }) {
-    
+    useEffect(() => {
+    const img = new Image();
+    img.src = guest_body_bg;
+
+    img.onload = () => {      
+      document.body.style.transition = 'background-image 0.5s ease-in-out';
+      document.body.style.backgroundImage = `url('${img.src}')`;
+      document.body.style.backgroundSize = 'cover';
+      document.body.style.backgroundPosition = 'center';
+      document.body.style.backgroundRepeat = 'no-repeat';
+    };
+
+    return () => {
+      document.body.style.backgroundImage = '';
+    };
+  }, []);
     useEffect(() => {
             document.body.className = bodyClass;
         }, [bodyClass]);
